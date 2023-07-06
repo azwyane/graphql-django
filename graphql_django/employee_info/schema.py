68b75c045744f6b5b245d9133ac93c475fcdbf60
azwyane <debianbyte@gmail.com>
2023-07-06 15:14:14 +1000

    development feat(employee_info): implement pagination for data query

--- graphql_django/employee_info/schema.py
+++ graphql_django/employee_info/schema.py
@@ -1,5 +1,7 @@
 import graphene
 from employee_info.models import Person as db_Person
+from django.core.paginator import Paginator
+
 class Person(graphene.ObjectType):
     email = graphene.String(required=True)
     name = graphene.String(required=True)
@@ -20,7 +22,22 @@ class Query(graphene.ObjectType):
     people = graphene.List(Person, page=graphene.Int(), limit=graphene.Int())
 
     def resolve_people(self, info, page=None, limit=None):
-        return db_Person.objects.all().select_related('address')
+        # Set default values for page and limit if not provided
+        page = page or 1
+        limit = limit or 10
+
+        persons = db_Person.objects.all().select_related('address')
+        
+        # Create a Paginator object with the desired limit
+        paginator = Paginator(persons, limit)
+
+        # Validate the requested page number
+        total_pages = paginator.num_pages
+        if page < 1 or page > total_pages:
+            raise ValueError("Invalid page number")
+
+        requested_page = paginator.page(page)
+        return requested_page
 
 
 schema = graphene.Schema(query=Query)
