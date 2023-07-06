c7ec08a8a1e688cbe608b4b4a1b8f0ddd03f5720
azwyane <debianbyte@gmail.com>
2023-07-06 16:08:48 +1000

    development add(employee_info): test cases for query data validation

--- graphql_django/employee_info/tests.py
+++ graphql_django/employee_info/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from graphene.test import Client
+from employee_info.schema import schema
 
-# Create your tests here.
+
+class PeopleTestCase(TestCase):
+    def setUp(self):
+        self.client = Client(schema)
+        self.query = '''
+            query {
+                people(page: 1, limit: 10) {
+                    email
+                    name
+                    address {
+                        number
+                        street
+                        city
+                        state
+                    }
+                }
+            }
+        '''
+
+        self.response = self.client.execute(self.query)
+        self.data = self.response['data']
+
+
+    def test_people_is_a_list(self):
+        self.assertIsInstance(self.data['people'], list)
+    
+    def test_address_is_available(self):
+        peoples = self.data['people']
+        for person in peoples:
+            self.assertIn('address', person)
+
